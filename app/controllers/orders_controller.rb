aba1aa7570c35986da743642f1f03b70815adfd0
Rob Dev <robinsonlam94@gmail.com>
2015-09-06 21:51:55 +1000

    Add menu ordering using check boxes.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,18 @@
 class OrdersController < ApplicationController
-	before_action :calculate_total, :except => [:index]
+	# before_action :calculate_total, :except => [:index, :new]
   
   def index
   	@orders = Order.all
+  end
+
+  def new
+  	@order = Order.new
+  	@items = Item.all
+  end
+
+  def create
+  	order = Order.create(:user_id => @current_user.id, :item_ids => params[:order][:item_ids])
+  	redirect_to order_path(order)
   end
 
   def show
